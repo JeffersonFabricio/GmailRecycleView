2b48b83abb385707657e4c52fbca33295f1157e0
JeffersonFabricio <jeffersonfabricioma@gmail.com>
2019-12-03 00:14:18 -0300

    reorder itens of recyclerview

--- RecyclerViewGmail/app/src/main/java/fabricio/jefferson/recyclerviewgmail/MainActivity.kt
+++ RecyclerViewGmail/app/src/main/java/fabricio/jefferson/recyclerviewgmail/MainActivity.kt
@@ -3,6 +3,7 @@ package fabricio.jefferson.recyclerviewgmail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mooveit.library.Fakeit
 import fabricio.jefferson.recyclerviewgmail.model.email
 import fabricio.jefferson.recyclerviewgmail.model.fakeEmails
@@ -23,18 +24,52 @@ class MainActivity : AppCompatActivity() {
         recycler_view_main.adapter = adapter
         recycler_view_main.layoutManager = LinearLayoutManager(this)
 
-        fab.setOnClickListener{
+        fab.setOnClickListener {
             addEmail()
             recycler_view_main.scrollToPosition(0)
         }
 
+        val helper =
+            androidx.recyclerview.widget.ItemTouchHelper(
+                ItemTouchHelper(
+                    androidx.recyclerview.widget.ItemTouchHelper.UP
+                            or androidx.recyclerview.widget.ItemTouchHelper.DOWN,
+                    androidx.recyclerview.widget.ItemTouchHelper.LEFT
+                )
+            )
+
+        helper.attachToRecyclerView(recycler_view_main)
+
     }
 
-    fun addEmail(){
+    inner class ItemTouchHelper(dragDirs: Int, swipeDirs: Int) :
+        androidx.recyclerview.widget.ItemTouchHelper.SimpleCallback(
+            dragDirs, swipeDirs
+        ) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            val from = viewHolder.adapterPosition
+            val to = target.adapterPosition
+
+            Collections.swap(adapter.emails, from, to)
+            adapter.notifyItemMoved(from, to)
+
+            return true
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        }
+
+    }
+
+    fun addEmail() {
         val sdf = SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR")).parse(
             Fakeit.dateTime().dateFormatter()
         )
-        adapter.emails.add(0, email{
+        adapter.emails.add(0, email {
             stared = false
             unread = true
             user = Fakeit.name().firstName()
